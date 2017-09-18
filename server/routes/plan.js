89970c13af2872be21874110bf215931e0aaa0d4
Tim Lohmann <tim.lohmann@gmail.com>
2017-09-18 10:17:26 -0500

    moved some files and auth work

--- server/routes/plan.js
+++ server/routes/plan.js
@@ -25,31 +25,7 @@ router.get('/', function(req, res) {
     })
 });
 
-router.post('/', function(req, res){
-	console.log('message post was hit!:', req.body);
-	//Add an INSERT query
-	pool.connect(function(errorConnectingToDatabase, client, done){
-		if(errorConnectingToDatabase) {
-			// when connecting to database failed
-			console.log('Error connecting to database', errorConnectingToDatabase);
-			res.sendStatus(500);
-		} else {
-			// when connecting to database worked!
-			// query like this: UPDATE messages SET message='Have a really terrific day!' WHERE id=1;
-			client.query('INSERT INTO activitytracker (company, job, date, contact, next_steps, notes) VALUES ($1, $2, $3, $4, $5, $6, $7);',
-							[req.body.company, req.body.job, req.body.date, req.body.contact, req.body.next_steps, req.body.notes], 
-							function(errorMakingQuery, result) {
-				done();
-				if(errorMakingQuery) {
-					console.log('Error making database query', errorMakingQuery);
-					res.sendStatus(500);
-				} else {
-					res.sendStatus(200);
-				}
-			});
-		}
-	});
-});
+
 
 router.get('/qanda', function(req, res) {
 	console.log('GET route hit');
